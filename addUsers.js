1ef0b2e730dbe374ea3944acf1234efc26d7267e
J Perrin <perrinjack96@gmail.com>
2020-06-16 21:43:53 +0100

    now reads login details from page and verifies whether user exists. Redirect onwards or nowhere if there is error

--- addUsers.js
+++ addUsers.js
@@ -2,10 +2,10 @@ const { client } = require("./dbConfig");
 
 
 
-const getName = (first_name, last_name, email, username) => {
+const getName = (first_name, last_name, email, username, password) => {
     const text = {
-        text: 'INSERT INTO users(first_name, last_name, email, username) VALUES($1, $2, $3, $4) RETURNING id, first_name, last_name, email, username',
-        values: [first_name, last_name, email, username],
+        text: 'INSERT INTO users(first_name, last_name, email, username, password) VALUES($1, $2, $3, $4, $5) RETURNING id, first_name, last_name, email, username,password',
+        values: [first_name, last_name, email, username, password],
       }
 
       //code to perform particular action.
--- routes/spaces.js
+++ routes/spaces.js
@@ -1,27 +1,30 @@
 var express = require('express');
 var router = express.Router();
-const { client } = require("../dbConfig");
+const { client } = require('../dbConfig');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-
+router.get('/', function (req, res, next) {
   res.render('spaces', { title: 'MakersBnB' });
 });
 
-
-router.post('/',(req,res) => {
-
-  const { client } = require('../dbConfig')
+router.post('/', (req, res) => {
+  const { client } = require('../dbConfig');
   // const client = new Client()
   // client.connect()
-  client.query("SELECT * FROM users WHERE username = $1", ['elonmusk'], (err, res) => {
-    if (err) throw err
-    console.log(res)
-    client.end()
-  })
-  console.log(req.body);
-    console.log(res.row);
-  res.redirect('/users')
+  client.query(
+    'SELECT * FROM users WHERE username = $1 AND password = $2',
+    [req.body.username, req.body.password],
+    (err, response) => {
+      console.log(response.rows.length); ////number of matches. Must be unique.
+      // console.log(err);
+      // console.log(err ? err.stack : response.rows);
+      if (response.rows.length === 1) {
+        res.redirect('/spaces');
+      } else {
+        res.redirect('/login');
+      }
+    }
+  );
 });
 
 module.exports = router;
