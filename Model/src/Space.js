f91b44b630ab54612cfe5f01b191e414c9fcbcc4
Rachael <rachaelewins@hotmail.com>
2020-06-16 16:01:33 +0100

    how model should connect to DB

--- Model/src/Space.js
+++ Model/src/Space.js
@@ -1,13 +1,28 @@
+const { client } = require("../../dbConfig");
+
 class Space {
-  constructor() {
-    this.names = [];
+  constructor(client) {
+    this.client = client
   }
 
   add(name) {
-    this.names.push(name);
+    db_connect(`INSERT INTO spaces (name) VALUES ('${name}')`)
   }
 
   list() {
-    return this.names;
+    db_connect(`SELECT name FROM spaces`)
+  }
+
+  dbConnect(string) {
+    console.log(string)
+    this.client.query(string, function(err, res) {
+      /*if (err) {
+        console.log(err.stack);
+      } else {
+       return res.rows;
+     } */
+
+   });
+    console.log(string)
   }
 }
