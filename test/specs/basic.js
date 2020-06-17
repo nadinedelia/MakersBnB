efdb4695fab856ea993cf955cf12b5b7e97f45dc
Nadine <nadinedelia@MacBook-Pro.local>
2020-06-17 17:09:53 +0200

    added feature tests

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -9,6 +9,10 @@ describe('Index page displays title', () => {
 describe('Failed Login', () => {
   it('Remains on login page', () => {
     browser.url('/login');
+    const username = $('#username');
+    const password = $('#password');
+    username.setValue('MileyCyrus');
+    password.setValue('molly1234');
     const mybutton = $('#button');
     mybutton.click();
     var url = browser.getUrl();
@@ -17,8 +21,27 @@ describe('Failed Login', () => {
   });
 });
 
+describe('Sign Up succeeded', () => {
+  it('Remains on signup page', () => {
+    browser.url('/signup');
+    const first_name = $('#first_name');
+    const last_name = $('#last_name');
+    const email = $('#email');
+    const username = $('#username');
+    const password = $('#password');
+    first_name.setValue('Donald');
+    last_name.setValue('Trump');
+    email.setValue('donald@trump.com');
+    username.setValue('TheRealDonald'); // fill in username and password fields.
+    password.setValue('123');
+    const mybutton = $('#button');
+    mybutton.click();
+    expect(browser).toHaveUrl('http://localhost:3000/users');
+  });
+});
+
 describe('Sucessful login', () => {
-  it('Moves onto spaces Page', () => {
+  xit('Moves onto spaces Page', () => {
     browser.url('/login');
     const username = $('#username');
     const password = $('#password');
