9a028dfca5b7686992a7af2cf0d5ce222a2833d6
Anthony <a.a.donovan@outlook.com>
2020-06-17 17:04:08 +0100

    updated spaces database for extra info

--- db/01_create_user.sql
+++ db/01_create_user.sql
@@ -1,2 +1,4 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(30), email VARCHAR(200), username VARCHAR(30));
-CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(30));
+CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(30), description TEXT, price NUMERIC(4,2), owner_id INTEGER);
+
+ALTER TABLE spaces ADD 
