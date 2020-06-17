f607531d68328e6676b4fcaa7acb062908bc238b
J Perrin <perrinjack96@gmail.com>
2020-06-17 13:40:50 +0100

    testing webdriver test framework, first custom test passing

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -5,3 +5,14 @@ describe('webdriver.io page', () => {
     expect(browser).toHaveTitle('MakersBnB');
   });
 });
+
+describe('webdriver.io page', () => {
+  it('should have the right title', () => {
+    browser.url('/login');
+    const mybutton = $('#button');
+    mybutton.click();
+    var url = browser.getUrl();
+    console.log('url');
+    expect(browser).toHaveUrl('http://localhost:3000/login');
+  });
+});
