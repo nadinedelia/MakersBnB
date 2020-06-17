4eb807a92763e6ed4c8f29b2b1b7f071e7894fac
J Perrin <perrinjack96@gmail.com>
2020-06-17 20:30:25 +0100

    refactored post route for sign up

--- routes/signup.js
+++ routes/signup.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const { client } = require('../dbConfig');
+var user = require('../addUsers');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* GET signup page. */
+router.get('/', function (req, res, next) {
   res.render('signup', { title: 'MakersBnB' });
 });
 
+router.post('/', (req, res) => {
+  user.getName(
+    req.body.first_name,
+    req.body.last_name,
+    req.body.email,
+    req.body.username,
+    req.body.password
+  ); //uses addUsers.js module
 
+  res.redirect('/spaces');
+});
 
 module.exports = router;
