8b0da23fca9f826c99ae73a5f8d41c3d34a18df9
J Perrin <perrinjack96@gmail.com>
2020-06-17 10:45:39 +0100

    login refactored so it posts to /login route. Login check refactored into checkLogin.js module

---
+++ checkLogin.js
@@ -0,0 +1,21 @@
+const { client } = require("./dbConfig");
+
+
+  const checkLogin = (username, password,res) => {
+    client.query(
+      'SELECT * FROM users WHERE username = $1 AND password = $2',
+      [username, password],
+      (err, response) => {
+        console.log(response.rows.length); ////number of matches. Must be unique.
+        if (response.rows.length === 1) {
+          res.redirect('/spaces'); //if successful redirect to spaces.
+        } else {
+          res.redirect('/login'); // if unsuccessful redirect to login route. 
+        }
+      }
+    );
+  };
+
+ exports.checkLogin = checkLogin;
+
+ //Exported module used during Login in login route. 
--- routes/spaces.js
+++ routes/spaces.js
@@ -7,24 +7,6 @@ router.get('/', function (req, res, next) {
   res.render('spaces', { title: 'MakersBnB' });
 });
 
-router.post('/', (req, res) => {
-  const { client } = require('../dbConfig');
-  // const client = new Client()
-  // client.connect()
-  client.query(
-    'SELECT * FROM users WHERE username = $1 AND password = $2',
-    [req.body.username, req.body.password],
-    (err, response) => {
-      console.log(response.rows.length); ////number of matches. Must be unique.
-      // console.log(err);
-      // console.log(err ? err.stack : response.rows);
-      if (response.rows.length === 1) {
-        res.redirect('/spaces');
-      } else {
-        res.redirect('/login');
-      }
-    }
-  );
-});
+
 
 module.exports = router;
