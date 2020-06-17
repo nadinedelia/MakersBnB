ae6d468877e9349d1ba7aa240e7a8da6399eb0d0
Anthony <a.a.donovan@outlook.com>
2020-06-17 23:42:23 +0100

    fixed table price size and delete button

--- db/01_create_user.sql
+++ db/01_create_user.sql
@@ -1,4 +1,4 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(30), email VARCHAR(200), username VARCHAR(30));
-CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(30), description TEXT, price NUMERIC(4,2), owner_id INTEGER);
+CREATE TABLE spaces (id SERIAL PRIMARY KEY, name VARCHAR(30), description TEXT, price NUMERIC(6,2), owner_id INTEGER);
 
 
