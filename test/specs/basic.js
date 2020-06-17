b7bbea4a1a5d276f41b459ec8b5e874956d0b13d
J Perrin <perrinjack96@gmail.com>
2020-06-17 14:29:39 +0100

    2rd sucessful login test runs and passes

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,18 +1,31 @@
-describe('webdriver.io page', () => {
-  it('should have the right title', () => {
+describe('Index page displays title', () => {
+  it('Makers BnB', () => {
     browser.url('/');
     const title = browser.getTitle();
     expect(browser).toHaveTitle('MakersBnB');
   });
 });
 
-describe('webdriver.io page', () => {
-  it('should have the right title', () => {
+describe('Failed Login', () => {
+  it('Remains on login page', () => {
     browser.url('/login');
     const mybutton = $('#button');
     mybutton.click();
     var url = browser.getUrl();
     console.log('url');
     expect(browser).toHaveUrl('http://localhost:3000/login');
+  });
+});
+
+describe('Sucessful login', () => {
+  it('Moves onto spaces Page', () => {
+    browser.url('/login');
+    const username = $('#username');
+    const password = $('#password');
+    username.setValue('Pezzer101'); // fill in username and password fields.
+    password.setValue('123');
+    const mybutton = $('#button');
+    mybutton.click();
+    expect(browser).toHaveUrl('http://localhost:3000/spaces');
   });
 });
