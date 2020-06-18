f7a6bbd5a57db5baf8a62cddbdf6eb3b7dc9b1e0
Rachael <rachaelewins@hotmail.com>
2020-06-18 14:55:54 +0100

    removed console.log

--- routes/newSpace.js
+++ routes/newSpace.js
@@ -4,12 +4,10 @@ var space  = require("../spaceModel");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log(req.session, 'new, get')
   res.render('newSpace', { title: 'MakersBnB' });
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.session, 'new, post')
   space.add(req.body, req.session.user);
   res.redirect('/spaces')
 });
