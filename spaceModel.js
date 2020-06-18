971b9064f48323e36fd6ed255063732fd572a57f
Rachael <rachaelewins@hotmail.com>
2020-06-18 17:31:00 +0100

    dates in spaces DB, skeleton for views

--- spaceModel.js
+++ spaceModel.js
@@ -2,8 +2,8 @@ const { client } = require("./dbConfig");
 
 const add = function(body, owner) {
   const text = {
-    text: 'INSERT INTO spaces (name, description, price, owner_id) VALUES($1, $2, $3, $4)',
-    values: [body.name, body.description, body.price, owner],
+    text: 'INSERT INTO spaces (name, description, price,date, owner_id) VALUES($1, $2, $3, $4, $5)',
+    values: [body.name, body.description, body.price, body.date, owner],
   }
   client.query(text, (err) => {
     if (err) {
@@ -29,6 +29,12 @@ async function getSpaces(id) {
   return spaces;
 }
 
+async function getDates(id) {
+  const dates =  await client.query('SELECT * FROM spaces WHERE id = $1', [id])
+  return dates.rows[0];
+}
+
 exports.add = add;
 exports.getSpaces = getSpaces;
+exports.getDates = getDates;
 exports.book = book;
