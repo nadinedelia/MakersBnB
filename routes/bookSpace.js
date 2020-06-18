4f427fa383d7eb3babbda2633ceb8c25ef37150d
J Perrin <perrinjack96@gmail.com>
2020-06-18 17:42:54 +0100

    new bookings save to a bookings table

--- routes/bookSpace.js
+++ routes/bookSpace.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var space  = require("../spaceModel");
+var space = require('../spaceModel');
 
-router.post('/:id', function(req, res, next) {
+router.post('/:id', function (req, res, next) {
+  // console.log(req.params);
+  console.log(space.booking(req.params.id));
   space.book(req.params.id);
-  res.redirect('/spaces')
+
+  res.redirect('/spaces');
 });
 
 module.exports = router;
--- spaceModel.js
+++ spaceModel.js
@@ -1,10 +1,11 @@
-const { client } = require("./dbConfig");
+const { client } = require('./dbConfig');
 
-const add = function(body, owner) {
+const add = function (body, owner) {
   const text = {
-    text: 'INSERT INTO spaces (name, description, price, owner_id) VALUES($1, $2, $3, $4)',
+    text:
+      'INSERT INTO spaces (name, description, price, owner_id) VALUES($1, $2, $3, $4)',
     values: [body.name, body.description, body.price, owner],
-  }
+  };
   client.query(text, (err) => {
     if (err) {
       console.log(err.stack);
@@ -12,23 +13,39 @@ const add = function(body, owner) {
   });
 };
 
-const book = function(id) {
+const booking = (id) => {
+  client.query('SELECT * FROM spaces WHERE id = $1', [id], (err, response) => {
+    console.log(response.rows[0]);
+    const text = {
+      text: 'INSERT INTO bookings (name, space_id, date) VALUES($1, $2, $3)',
+      values: [response.rows[0].name, response.rows[0].id, response.rows[0].dates],
+    };
+    client.query(text, (err) => {
+      if (err) {
+        console.log(err.stack);
+      }
+    });
+  });
+};
+
+const book = function (id) {
   const text = {
     text: 'DELETE FROM spaces WHERE id = $1',
     values: [id],
-  }
+  };
   client.query(text, (err) => {
     if (err) {
       console.log(err.stack);
     }
   });
-}
+};
 
 async function getSpaces() {
-  const spaces =  await client.query('SELECT * FROM spaces')
+  const spaces = await client.query('SELECT * FROM spaces');
   return spaces;
 }
 
+exports.booking = booking;
 exports.add = add;
 exports.getSpaces = getSpaces;
 exports.book = book;
