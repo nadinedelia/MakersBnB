fa5780a72c4107433233a51a3e142f935157a92a
Rachael <rachaelewins@hotmail.com>
2020-06-18 15:36:52 +0100

    login logic also puts user data into session

--- checkLogin.js
+++ checkLogin.js
@@ -1,23 +1,13 @@
 const { client } = require("./dbConfig");
 
 
-  const checkLogin = (username, password,res) => {
-    client.query(
+  async function checkLogin(username, password, res)  {
+    const toReturn = await client.query(
       'SELECT * FROM users WHERE username = $1 AND password = $2',
-      [username, password],
-      (err, response) => {
-         ////number of matches. Must be unique.
-        if (response.rows.length === 1) {
-            console.log('LOGIN SUCCESS');
-          res.redirect('/spaces'); //if successful redirect to spaces.
-        } else {
-          res.redirect('/login'); // if unsuccessful redirect to login route. 
-          console.log('LOGIN FAIL');
-        }
-      }
-    );
+      [username, password]);
+    return toReturn;
   };
 
  exports.checkLogin = checkLogin;
 
- //Exported module used during Login in login route. 
+ //Exported module used during Login in login route.
