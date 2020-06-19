9b666afce02641feb1b4f29f0928be836311e921
J Perrin <perrinjack96@gmail.com>
2020-06-19 14:40:48 +0100

    tests all passing with test database

--- checkLogin.js
+++ checkLogin.js
@@ -1,13 +1,14 @@
-const { client } = require("./dbConfig");
+const { client } = require('./dbConfig');
 
+async function checkLogin(username, password, res) {
+  const toReturn = await client.query(
+    'SELECT * FROM users WHERE username = $1 AND password = $2',
+    [username, password]
+  );
+  console.log(toReturn);
+  return toReturn;
+}
 
-  async function checkLogin(username, password, res)  {
-    const toReturn = await client.query(
-      'SELECT * FROM users WHERE username = $1 AND password = $2',
-      [username, password]);
-    return toReturn;
-  };
+exports.checkLogin = checkLogin;
 
- exports.checkLogin = checkLogin;
-
- //Exported module used during Login in login route.
+//Exported module used during Login in login route.
